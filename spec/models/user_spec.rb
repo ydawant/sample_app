eeeabfc1c612ff173cadb02418ddcd9129102a5f
Yannick Dawant <ydawant@gmail.com>
2013-01-08 19:13:14 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+
   end
 
   subject { @user }
@@ -24,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -118,4 +120,10 @@ end
     specify { user_for_invalid_password.should be_false }
   end
 end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
